eef92e201efd14f928dcdd769ebba1b0fc25bb11
MikeMG-PL <36279247+MikeMG-PL@users.noreply.github.com>
2020-11-20 22:27:52 +0100

    6.1 - node system graph (needs fixes)

---
+++ Susteny/Assets/Scripts/DialogueNodeSystem/Editor/DialogueGraph.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+using UnityEditor.Experimental.GraphView;
+using System;
+
+public class DialogueGraph : GraphViewEditorWindow
+{
+    DialogueGraphView _graphView;
+    string _filename = "New Narrative";
+
+    [MenuItem("Graph/Dialogue Graph")]
+    public static void OpenDialogueGraphWindow()
+    {
+        var window = GetWindow<DialogueGraph>();
+        window.titleContent = new GUIContent(text: "Dialogue Graph");
+    }
+
+    void OnEnable()
+    {
+        ConstructGraphView();
+        GenerateToolbar();
+    }
+
+    void ConstructGraphView()
+    {
+        _graphView = new DialogueGraphView
+        {
+            name = "Dialogue Graph"
+        };
+
+        _graphView.StretchToParentSize();
+        rootVisualElement.Add(_graphView);
+    }
+
+    void GenerateToolbar()
+    {
+        var toolbar = new Toolbar();
+
+        var fileNameTextField = new TextField(label: "File Name:");
+        fileNameTextField.SetValueWithoutNotify(_filename);
+        fileNameTextField.MarkDirtyRepaint();
+        fileNameTextField.RegisterCallback((EventCallback<ChangeEvent<string>>)(evt => _filename = evt.newValue));
+        toolbar.Add(fileNameTextField);
+
+        var nodeCreateButton = new Button(clickEvent: () => { _graphView.CreateNode("Dialogue Node"); });
+        nodeCreateButton.text = "Create node";
+
+        toolbar.Add(nodeCreateButton);
+
+        toolbar.Add(child: new Button(clickEvent: () => RequestDataOperation(true)) { text = "Save Data" });
+        toolbar.Add(child: new Button(clickEvent: () => RequestDataOperation(false)) { text = "Load Data" });
+
+        rootVisualElement.Add(toolbar);
+    }
+
+    void RequestDataOperation(bool save)
+    {
+        if (string.IsNullOrEmpty(_filename))
+            EditorUtility.DisplayDialog("Invalid file name!", "Please enter a valid file name.", "OK");
+
+        var saveUtility = GraphSaveUtility.GetInstance(_graphView);
+
+        if (save)
+            saveUtility.SaveGraph(_filename);
+        else
+            saveUtility.LoadGraph(_filename);
+    }
+
+    void OnDisable()
+    {
+        rootVisualElement.Remove(_graphView);
+    }
+}
---
+++ Susteny/Assets/Scripts/DialogueNodeSystem/Editor/GraphSaveUtility.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor.Experimental.GraphView;
+using UnityEditor;
+using UnityEngine;
+
+public class GraphSaveUtility
+{
+    DialogueGraphView _targetGraphView;
+    DialogueContainer _containerCache;
+
+    List<Edge> Edges => _targetGraphView.edges.ToList();
+    List<DialogueNode> Nodes => _targetGraphView.nodes.ToList().Cast<DialogueNode>().ToList();
+
+    public static GraphSaveUtility GetInstance(DialogueGraphView targetGraphView)
+    {
+        return new GraphSaveUtility
+        {
+            _targetGraphView = targetGraphView
+        };
+    }
+
+    public void SaveGraph(string fileName)
+    {
+        if (!Edges.Any()) return;
+        var dialogueContainer = ScriptableObject.CreateInstance<DialogueContainer>();
+        var connectedPorts = Edges.Where(Edge => Edge.input.node != null).ToArray();
+
+        for(int i = 0; i<connectedPorts.Length; i++)
+        {
+            var outputNode = connectedPorts[i].output.node as DialogueNode;
+            var inputNode = connectedPorts[i].input.node as DialogueNode;
+
+            dialogueContainer.NodeLinks.Add(new NodeLinkData
+            {
+                BaseNodeGuid = outputNode.GUID,
+                PortName = connectedPorts[i].output.portName,
+                TargetNodeGuid = inputNode.GUID
+            });
+        }
+
+        foreach(var dialogueNode in Nodes.Where(node=>!node.EntryPoint))
+        {
+            dialogueContainer.DialogueNodeData.Add(new DialogueNodeData
+            {
+                Guid = dialogueNode.GUID,
+                DialogueText = dialogueNode.DialogueText,
+                Position = dialogueNode.GetPosition().position
+            });
+        }
+
+        if (!AssetDatabase.IsValidFolder("Assets/Resources/Dialogues"))
+            AssetDatabase.CreateFolder("Assets", "Resources");
+
+        AssetDatabase.CreateAsset(dialogueContainer, $"Assets/Resources/Dialogues/{fileName}.asset");
+        AssetDatabase.SaveAssets();
+    }
+
+    public void LoadGraph(string fileName)
+    {
+        _containerCache = Resources.Load<DialogueContainer>($"Dialogues/{fileName}");
+        if(_containerCache == null)
+        {
+            EditorUtility.DisplayDialog("File not found", "Target dialogue graph file does not exist.", "OK");
+            return;
+        }
+
+        ClearGraph();
+        CreateNodes();
+        ConnectNodes();
+    }
+
+    void ClearGraph()
+    {
+        Nodes.Find(DialogueNode => DialogueNode.EntryPoint).GUID = _containerCache.NodeLinks[0].BaseNodeGuid;
+        foreach (var node in Nodes)
+        {
+            if (node.EntryPoint) continue;
+            Edges.Where(Edges => Edges.input.node == node).ToList().ForEach(edge=>_targetGraphView.RemoveElement(edge));
+
+            _targetGraphView.RemoveElement(node);
+        }
+    }
+
+    void CreateNodes()
+    {
+        foreach(DialogueNodeData nodeData in _containerCache.DialogueNodeData)
+        {
+            var tempNode = _targetGraphView.CreateDialogueNode(nodeData.DialogueText);
+            tempNode.GUID = nodeData.Guid;
+            _targetGraphView.AddElement(tempNode);
+
+            var nodePorts = _containerCache.NodeLinks.Where(NodeLinkData => NodeLinkData.BaseNodeGuid == nodeData.Guid).ToList();
+            nodePorts.ForEach(NodeLinkData => _targetGraphView.AddChoicePort(tempNode, NodeLinkData.PortName));
+        }
+    }
+
+    void ConnectNodes()
+    {
+
+    }
+}
