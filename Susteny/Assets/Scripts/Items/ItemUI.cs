bd0ba62592edb1e803343559f48e0adcca63660d
Mikołaj Przybylski <clovers02123@gmail.com>
2020-12-10 00:03:39 +0100

    6.3 Viewing item model from inventory
    + some bug fixes, changes in item prefabs
    + enabling and disabling inventory, player can't move while browsing inventory

---
+++ Susteny/Assets/Scripts/Items/ItemUI.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemUI : MonoBehaviour
+{
+    public Item item;
+    public ViewMode viewMode;
+
+    public void EnableViewMode()
+    {
+        viewMode.ToggleViewInventoryItem(true, item);
+    }
+}
