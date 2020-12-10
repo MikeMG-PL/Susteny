2b780ed1093569d7e28f40941f69209cba6dbcaf
MikeMG-PL <36279247+MikeMG-PL@users.noreply.github.com>
2020-12-10 10:20:44 +0100

    6.6 | NPC on the street

---
+++ Susteny/Assets/Scripts/NPCs/WalkAround.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WalkAround : MonoBehaviour
+{
+    Camera cam;
+    NavMeshAgent agent;
+    GameObject navigationPointsContainer;
+    SC_FPSController player;
+
+    int point;      // Do którego punktu zmierzać będzie NPC
+    float time;     // Ile ma trwać jego podróż, zanim ją przerwie i zmieni punkt
+    float speed;    // Szybkość chodu NPC;
+    int priority; // Priorytet omijania przeszkód wzgl. innych NPC
+    /////////////////////////////////////////////////////////////////////////
+    float timer;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<SC_FPSController>();
+        cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        agent = GetComponent<NavMeshAgent>();
+        navigationPointsContainer = GameObject.FindGameObjectWithTag("NavigationPoints");
+        Destination();
+    }
+
+    void Destination()
+    {
+        point = Random.Range(0, navigationPointsContainer.transform.childCount);
+        time = Random.Range(10, 40);
+        speed = Random.Range(3, 4);
+        priority = Random.Range(0, 99);
+        Move();
+    }
+
+    void Move()
+    {
+        agent.SetDestination(navigationPointsContainer.transform.GetChild(point).position);
+        agent.speed = speed;
+        agent.avoidancePriority = priority;
+        timer = 0;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GetComponent<CapsuleCollider>().radius = GetComponent<CapsuleCollider>().radius * 0.1f;
+            agent.radius = agent.radius * 0.0001f;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GetComponent<CapsuleCollider>().radius = GetComponent<CapsuleCollider>().radius / 0.1f;
+            agent.radius = agent.radius / 0.0001f;
+        }
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (Vector3.Distance(agent.pathEndPosition, agent.transform.position) < 4 || timer >= time)
+            Destination();
+    }
+}
