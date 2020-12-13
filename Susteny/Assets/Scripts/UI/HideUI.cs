737e872963ed749798999e1779df5ff61093f99d
MikeMG-PL <36279247+MikeMG-PL@users.noreply.github.com>
2020-12-13 15:53:03 +0100

    Crosshair tweak, yay!

--- Susteny/Assets/Scripts/UI/HideUI.cs
+++ Susteny/Assets/Scripts/UI/HideUI.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HideUI : MonoBehaviour
 {
+    public Image crosshair;
+    Color currentColor;
+
     void Start()
     {
         DialogueInteraction.Talking += HideOverlayingUI;
         ViewMode.Viewing += HideOverlayingUI;
         Prototype.LevelStart += HideOverlayingUI;
         PlayerActions.BrowsingInventory += HideOverlayingUI;
+
+        currentColor = crosshair.color;
     }
 
     void OnDisable()
@@ -22,9 +28,32 @@ public class HideUI : MonoBehaviour
 
     void HideOverlayingUI(bool b)
     {
-        for(int i = 0; i < transform.childCount; i++)
+        for (int i = 0; i < transform.childCount; i++)
         {
             transform.GetChild(i).gameObject.SetActive(!b);
         }
+    }
+
+    RaycastHit _hit = new RaycastHit();
+    Ray ray;
+
+    void Update()
+    {
+        LookRay();
+    }
+
+    void LookRay()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out _hit, 20))
+        {
+            if (_hit.transform.GetComponent<ItemWorld>() != null || _hit.transform.GetComponent<LoadDialogue>() != null)
+                crosshair.color = Color.green;
+            else
+                crosshair.color = currentColor;
+        }
+        else
+            crosshair.color = currentColor;
     }
 }
