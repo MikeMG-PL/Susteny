14b0fc2770c98edbd840290790ea9eba2264a3b0
Mikołaj Przybylski <clovers02123@gmail.com>
2020-12-15 22:04:40 +0100

    7.0 Updated crosshair
    - changing crosshair works with children of items
    - checking if interaction distance is correct
    - optimizations

---
+++ Susteny/Assets/Scripts/UI/Crosshair.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Crosshair : MonoBehaviour
+{
+    public Image crosshair;
+
+    new Camera camera;
+    GameObject player;
+    GameObject lastHit;
+    Color defaultColor;
+    Color interactColor = Color.green;
+    RaycastHit _hit = new RaycastHit();
+    Ray ray;
+    bool hitInteractable;
+    float interactionDistance;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        camera = Camera.main;
+
+        defaultColor = crosshair.color;
+    }
+
+    void Update()
+    {
+        LookRay();
+    }
+
+    void LookRay()
+    {
+        ray = camera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out _hit, 20))
+        {
+            // Jeśli *nadal* patrzymy na coś z czym możemy przeprowadzić interakcję, ale odsunęliśmy się za daleko lub przybliżyliśmy się
+            if (ReferenceEquals(lastHit, _hit.transform.gameObject) && hitInteractable)
+            {
+                float distance = Vector3.Distance(_hit.transform.position, player.transform.position);
+                if (distance > interactionDistance) crosshair.color = defaultColor;
+                else crosshair.color = interactColor;
+            }
+
+            // Jeśli *nadal* patrzymy na ten sam obiekt, ale nie jest on interactable
+            else if (ReferenceEquals(lastHit, _hit.transform.gameObject) && !hitInteractable) crosshair.color = defaultColor;
+
+            // Jeśli patrzymy na inny obiekt
+            else
+            {
+                lastHit = _hit.transform.gameObject;
+
+                CheckIfHitInteractable(_hit.transform);
+
+                if (hitInteractable)
+                {
+                    float distance = Vector3.Distance(_hit.transform.position, player.transform.position);
+                    if (distance <= interactionDistance) crosshair.color = interactColor;
+                }
+
+                else
+                    crosshair.color = defaultColor;
+            }
+
+        }
+
+        else
+        {
+            lastHit = null;
+            crosshair.color = defaultColor;
+        }
+
+    }
+
+    // Sprawdzanie czy natknięto się na przedmiot, lub osobę, z którą można wejść w interakcję
+    void CheckIfHitInteractable(Transform _transform)
+    {
+        hitInteractable = true;
+
+        if (_transform.GetComponent<ItemWorld>() != null)
+            interactionDistance = _transform.GetComponent<ItemWorld>().interactionDistance;
+
+        else if (_transform.GetComponent<ChildTriggerItemAction>() != null)
+            interactionDistance = _transform.GetComponentInParent<ItemWorld>().interactionDistance;
+
+        else if (_transform.GetComponent<LoadDialogue>() != null)
+            interactionDistance = _transform.GetComponent<DialogueInteraction>().interactionDistance;
+
+        else
+            hitInteractable = false;
+    }
+}
--- Susteny/Assets/Scripts/UI/HideUI.cs
+++ Susteny/Assets/Scripts/UI/HideUI.cs
@@ -5,17 +5,12 @@ using UnityEngine.UI;
 
 public class HideUI : MonoBehaviour
 {
-    public Image crosshair;
-    Color currentColor;
-
     void Start()
     {
         DialogueInteraction.Talking += HideOverlayingUI;
         ViewMode.ViewingItem += HideOverlayingUI;
         Prototype.LevelStart += HideOverlayingUI;
         PlayerActions.BrowsingInventory += HideOverlayingUI;
-
-        currentColor = crosshair.color;
     }
 
     void OnDisable()
@@ -32,28 +27,5 @@ public class HideUI : MonoBehaviour
         {
             transform.GetChild(i).gameObject.SetActive(!b);
         }
-    }
-
-    RaycastHit _hit = new RaycastHit();
-    Ray ray;
-
-    void Update()
-    {
-        LookRay();
-    }
-
-    void LookRay()
-    {
-        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        if (Physics.Raycast(ray, out _hit, 20))
-        {
-            if (_hit.transform.GetComponent<ItemWorld>() != null || _hit.transform.GetComponent<LoadDialogue>() != null)
-                crosshair.color = Color.green;
-            else
-                crosshair.color = currentColor;
-        }
-        else
-            crosshair.color = currentColor;
     }
 }
