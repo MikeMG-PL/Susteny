3df9080d825abe01a0355a713d0a9b2ace220526
MikeMG-PL <36279247+MikeMG-PL@users.noreply.github.com>
2020-12-28 17:08:24 +0100

    8.6 Imported Cinemachine

---
+++ Susteny/Assets/Scripts/Management/Level system/Choices_Consequences.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Choices_Consequences : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
