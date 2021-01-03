224c3eec48cbfe00fecd46ba87702c96348ec8a1
MikeMG-PL <36279247+MikeMG-PL@users.noreply.github.com>
2021-01-03 20:17:34 +0100

    8.5 | Final scene tweak

--- Susteny/Assets/1.SCRIPTS/NPCs/Characters/Anna.cs
+++ Susteny/Assets/1.SCRIPTS/NPCs/Characters/Anna.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 using UnityEngine.AI;
 
 public class Anna : MonoBehaviour
 {
     public List<Transform> destinations;
     NavMeshAgent agent;
+
+    public PlayableDirector timeline2;
 
     private void Awake()
     {
@@ -16,5 +19,11 @@ public class Anna : MonoBehaviour
     public void FindPlayer()
     {
         agent.SetDestination(destinations[0].position);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == destinations[0])
+            timeline2.Play();
     }
 }
