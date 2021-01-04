f0f1ccc1685823cc93d84968cc799a57b35eb75d
MikeMG-PL <36279247+MikeMG-PL@users.noreply.github.com>
2021-01-04 22:54:24 +0100

    8.5 Progress in prototype using Timeline

--- Susteny/Assets/1.SCRIPTS/NPCs/Characters/Anna.cs
+++ Susteny/Assets/1.SCRIPTS/NPCs/Characters/Anna.cs
@@ -7,13 +7,22 @@ using UnityEngine.AI;
 public class Anna : MonoBehaviour
 {
     public List<Transform> destinations;
+    public GameObject player;
+    public Item photoItem;
     NavMeshAgent agent;
 
     public PlayableDirector timeline2;
+    public PlayableDirector timeline3;
 
-    private void Awake()
+    void Awake()
     {
+        DialogueInteraction.Conversation += AnnaConversationStart;
         agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void OnDisable()
+    {
+        DialogueInteraction.Conversation -= AnnaConversationStart;
     }
 
     public void FindPlayer()
@@ -31,4 +40,35 @@ public class Anna : MonoBehaviour
     {
         GetComponent<LoadDialogue>().currentDialogueID = i;
     }
+
+    public void AnnaConversationStart(bool start, string objSpeakerName, int ID)
+    {
+        if(objSpeakerName == "Anna")
+        {
+            switch(ID)
+            {
+                default:
+                    break;
+
+                case 1:
+                    if(!start)
+                    {
+                        timeline3.Play();
+                    }    
+                    break;
+            }
+        }
+    }
+
+    public void GivePhoto()
+    {
+        player.GetComponent<Inventory>().Add(photoItem, 1);
+    }
+
+    public void FinishPhotoViewing()
+    {
+        player.GetComponent<PlayerActions>().UngrabFromInventory();
+    }
+
+    // debugging part
 }
