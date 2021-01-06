b21719d7652b879b161c0340976366416527dfc0
Mikołaj Przybylski <clovers02123@gmail.com>
2021-01-06 20:36:25 +0100

    Different oil painting shader

---
+++ Susteny/Assets/Resources/PaintEffect.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+[ExecuteInEditMode]
+public class PaintEffect : MonoBehaviour
+{
+	public int intensity;
+	private Material material;
+
+	void Awake()
+	{
+		// Create a new material with the supplied shader.
+		material = new Material(Shader.Find("SMO/Complete/Painting"));
+	}
+
+	// OnRenderImage() is called when the camera has finished rendering.
+	void OnRenderImage(RenderTexture src, RenderTexture dst)
+	{
+		if (intensity == 0)
+		{
+			Graphics.Blit(src, dst);
+			return;
+		}
+
+		material.SetInt("_KernelSize", intensity);
+		Graphics.Blit(src, dst, material);
+	}
+}
