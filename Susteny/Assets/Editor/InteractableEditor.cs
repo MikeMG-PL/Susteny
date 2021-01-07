59f9dc0bfef77a98d618a4fb705bf9127e95b079
Mikołaj Przybylski <clovers02123@gmail.com>
2021-01-07 15:06:46 +0100

    Small fix + tips in Interactable editor

--- Susteny/Assets/Editor/InteractableEditor.cs
+++ Susteny/Assets/Editor/InteractableEditor.cs
@@ -28,7 +28,7 @@ public class InteractableEditor : Editor
         DrawDefaultInspector();
         Interactable interactable = (Interactable)target;
 
-        EditorGUILayout.PropertyField(enableGoTo, new GUIContent("Walk to the object"));
+        EditorGUILayout.PropertyField(enableGoTo, new GUIContent("Walk to the object", "If the transform is null, the default position will be chosen"));
 
         if (interactable.enableGoTo)
         {
@@ -37,11 +37,10 @@ public class InteractableEditor : Editor
             EditorGUILayout.Space(3f);
         }
 
-        EditorGUILayout.PropertyField(enableLookAt, new GUIContent("Look at object"));
+        EditorGUILayout.PropertyField(enableLookAt, new GUIContent("Look at object", "Leave transform null, if you want player to look at interactable obj itself"));
 
         if (interactable.enableLookAt)
         {
-            if (objectToLookAt.objectReferenceValue == null) objectToLookAt.objectReferenceValue = interactable.transform;
             EditorGUILayout.PropertyField(objectToLookAt, new GUIContent("Transform", "Player will look at the center of this object during interaction"));
 
             EditorGUILayout.Space(3f);
