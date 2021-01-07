cc571ba5ebfc0fb7f78e9e39d1fdd1c619298243
Mikołaj Przybylski <clovers02123@gmail.com>
2021-01-07 14:23:29 +0100

    Remade custom inspector of Interactable with Serialized Properties, moved it to its own file, moved the file to the Editor folder

---
+++ Susteny/Assets/Editor/InteractableEditor.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Interactable))]
+[CanEditMultipleObjects]
+public class InteractableEditor : Editor
+{
+    SerializedProperty positionToGo;
+    SerializedProperty objectToLookAt;
+    SerializedProperty cursorOnWhenOnPosition;
+    SerializedProperty enableLookAt;
+    SerializedProperty enableGoTo;
+
+    private void OnEnable()
+    {
+        positionToGo = serializedObject.FindProperty("positionToGo");
+        objectToLookAt = serializedObject.FindProperty("objectToLookAt");
+        cursorOnWhenOnPosition = serializedObject.FindProperty("cursorOnWhenOnPosition");
+        enableLookAt = serializedObject.FindProperty("enableLookAt");
+        enableGoTo = serializedObject.FindProperty("enableGoTo");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        DrawDefaultInspector();
+        Interactable interactable = (Interactable)target;
+
+        EditorGUILayout.PropertyField(enableGoTo, new GUIContent("Walk to the object"));
+
+        if (interactable.enableGoTo)
+        {
+            EditorGUILayout.PropertyField(positionToGo, new GUIContent("Transform", "Player will stand at this position during interaction"));
+
+            EditorGUILayout.Space(3f);
+        }
+
+        EditorGUILayout.PropertyField(enableLookAt, new GUIContent("Look at object"));
+
+        if (interactable.enableLookAt)
+        {
+            if (objectToLookAt.objectReferenceValue == null) objectToLookAt.objectReferenceValue = interactable.transform;
+            EditorGUILayout.PropertyField(objectToLookAt, new GUIContent("Transform", "Player will look at the center of this object during interaction"));
+
+            EditorGUILayout.Space(3f);
+        }
+
+
+        if (interactable.enableGoTo || interactable.enableLookAt)
+        {
+            EditorGUILayout.PropertyField(cursorOnWhenOnPosition, new GUIContent("Cursor enabled after player on position"));
+            EditorGUILayout.Space(3f);
+        }
+        else cursorOnWhenOnPosition.boolValue = false;
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
