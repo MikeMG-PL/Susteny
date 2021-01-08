93cd29e8b471b12d79c6dbc0d488ed0cbc681531
MikeMG-PL <36279247+MikeMG-PL@users.noreply.github.com>
2021-01-08 19:02:25 +0100

    Fixing (not ready) disabling viewmode in opening scene

--- Susteny/Assets/4.LEVELS/Scripts/Prototype/Prototype.cs
+++ Susteny/Assets/4.LEVELS/Scripts/Prototype/Prototype.cs
@@ -1,13 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class Prototype : MonoBehaviour
 {
     public TaskSystem tasks;
+    public PlayableDirector timeline5;
 
+    /// SUBSCRIBING EVENTS ///
+    void Awake()
+    {
+        ViewMode.ViewingItem += OnViewModeSwitch;
+    }
+
+    /// UNSUBSCRIBING EVENTS ///
+    void OnDisable()
+    {
+        
+    }
+
+    /// FUNCTIONS ///
     public void AddTaskFromList(int i)
     {
         tasks.addTask(tasks.availableTasks[i]);
+    }
+
+    void OnViewModeSwitch(bool b)
+    {
+        if(!b)
+        {
+            Debug.Log("Off");
+        }
     }
 }
