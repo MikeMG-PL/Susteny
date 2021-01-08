1c1b7a8b749cfc8da4362d29c8741bb2726495f9
MikeMG-PL <36279247+MikeMG-PL@users.noreply.github.com>
2021-01-08 17:49:19 +0100

    8.5 Simplified hierarchy, tweaks, progress

---
+++ Susteny/Assets/4.LEVELS/Scripts/Prototype/Prototype.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prototype : MonoBehaviour
+{
+    public TaskSystem tasks;
+
+    public void AddTaskFromList(int i)
+    {
+        tasks.addTask(tasks.availableTasks[i]);
+    }
+}
