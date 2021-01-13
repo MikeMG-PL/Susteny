292182e396fdb1f127e06bc5a266c6fe44327893
MikeMG-PL <36279247+MikeMG-PL@users.noreply.github.com>
2021-01-13 21:06:51 +0100

    8.5 Progress in programming Door custom editor

---
+++ Susteny/Assets/Editor/OuterDoorEditor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(OuterDoor))]
+[CanEditMultipleObjects]
+public class OuterDoorEditor : Editor
+{
+    // declare serialized properties here
+
+    private void OnEnable()
+    {
+        // assign serialized properties here
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        // draw GUI here
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
