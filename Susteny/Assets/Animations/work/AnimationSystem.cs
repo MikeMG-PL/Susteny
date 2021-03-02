4200d897c5cb5beceb26e359793a2cf4ff1aa04e
Mateusz1010 <lolmanek123456@gmail.com>
2021-03-02 21:28:19 +0100

    animation controller made  properly (pleaseeee, let that be true)

--- Susteny/Assets/Animations/work/AnimationSystem.cs
+++ Susteny/Assets/Animations/work/AnimationSystem.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Animations.Rigging;
 
 public class AnimationSystem : MonoBehaviour
@@ -10,9 +8,6 @@ public class AnimationSystem : MonoBehaviour
     void Start()
     {
         animatorController = GetComponent<Animator>().runtimeAnimatorController;
-        GameObject ob = new GameObject("ob");
-        ob.transform.position = new Vector3(1, 1, -1.5f);
-        makeLookAt(ybot, ybot); // ----- usuń ----- //
     }
 
     public static Animator prepare(GameObject character)
@@ -33,47 +28,19 @@ public class AnimationSystem : MonoBehaviour
         return characterAnimator;
     }
 
-    public static RigBuilder prepareRig(GameObject character)
-    {
-        RigBuilder rigBuilder = character.GetComponent<RigBuilder>();
-
-        if (rigBuilder == null)
-        {
-            character.AddComponent<RigBuilder>();
-            GameObject headRig = new GameObject("headRig");
-            headRig.transform.SetParent(character.transform);
-            GameObject bodyRig = new GameObject("bodyRig");
-            bodyRig.transform.SetParent(character.transform);
-
-            headRig.AddComponent<Rig>();
-            bodyRig.AddComponent<Rig>().weight = 0.5f;
-
-            MultiAimConstraint headConstraint = headRig.AddComponent<MultiAimConstraint>();
-            MultiAimConstraint bodyConstraint = bodyRig.AddComponent<MultiAimConstraint>();
-
-            foreach (Transform data in character.GetComponentInChildren<Transform>())
-            {
-                if (data.name == "mixamorig:Hips")
-                {
-                    headConstraint.data.constrainedObject = data.Find("mixamorig:Spine").Find("mixamorig:Spine1").Find("mixamorig:Spine2").Find("mixamorig:Neck").Find("mixamorig:Head");
-                    bodyConstraint.data.constrainedObject = data.Find("mixamorig:Spine");
-                    break;
-                }
-            }
-
-            rigBuilder = character.GetComponent<RigBuilder>();
-
-            rigBuilder.layers.Add(new RigBuilder.RigLayer(headRig.GetComponent<Rig>(),true));
-            rigBuilder.layers.Add(new RigBuilder.RigLayer(bodyRig.GetComponent<Rig>(), true));
-            rigBuilder.Build();
-        }
-
-        return rigBuilder;
-    }
-
-    public static void animate(GameObject character, Animations animation)
+    public static void animate(GameObject character, Animations animation, AnimationBodyPart animationBodyPart)
     {
         Animator characterAnimator = prepare(character);
+        if(animationBodyPart == AnimationBodyPart.WHOLE_BODY)
+        {
+            characterAnimator.SetInteger("bodyLevel", 1);
+            characterAnimator.SetInteger("animationIndex", (int)animation);
+            //characterAnimator.SetTrigger("endTrigger");
+            characterAnimator.SetInteger("bodyLevel", 2);
+            characterAnimator.SetInteger("animationIndex", (int)animation);
+            characterAnimator.SetTrigger("endTrigger");
+        }
+        else characterAnimator.SetInteger("bodyLevel", (int)animationBodyPart);
         characterAnimator.SetInteger("animationIndex", (int) animation);
         characterAnimator.SetTrigger("endTrigger");
     }
@@ -86,20 +53,7 @@ public class AnimationSystem : MonoBehaviour
 
     public static void stopAnimation(GameObject character)
     {
-        animate(character, Animations.NULL);
-    }
-
-    public static void makeLookAt(GameObject character, GameObject observed)
-    {
-        prepare(character);
-        RigBuilder rigBuilder = prepareRig(character);
-        for (int i = 0; i < rigBuilder.layers.Count; i++)
-        {
-            WeightedTransform source = new WeightedTransform(observed.transform, 1);
-            //rigBuilder.layers[i].rig.GetComponentInParent<MultiAimConstraint>().data.constrainedObject = observed.transform;
-            rigBuilder.layers[i].rig.GetComponentInParent<MultiAimConstraint>().data.sourceObjects.Add(source);
-            rigBuilder.Build();
-        }
+        animate(character, Animations.NULL, AnimationBodyPart.WHOLE_BODY);
     }
 
     // -------------------------------------------------------------------- TESTING AND DEBUGING -------------------------------------------------------------------- //
@@ -110,11 +64,11 @@ public class AnimationSystem : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown("q")) AnimationSystem.animate(ybot, Animations.IDLE);
-        if (Input.GetKeyDown("w")) AnimationSystem.animate(ybot, Animations.RUN);
-        if (Input.GetKeyDown("e")) AnimationSystem.animate(ybot, Animations.DANCE);
-        if (Input.GetKeyDown("r")) AnimationSystem.animate(ybot, Animations.DIE);
-        if (Input.GetKeyDown("t")) AnimationSystem.animate(ybot, Animations.WALK_MEN);
+        if (Input.GetKeyDown("q")) AnimationSystem.animate(ybot, Animations.IDLE_1, AnimationBodyPart.UPPER_BODY);
+        if (Input.GetKeyDown("w")) AnimationSystem.animate(ybot, Animations.RUNNING_1, AnimationBodyPart.UPPER_BODY);
+        if (Input.GetKeyDown("e")) AnimationSystem.animate(ybot, Animations.SITTING_1, AnimationBodyPart.UPPER_BODY);
+        if (Input.GetKeyDown("r")) AnimationSystem.animate(ybot, Animations.DYING, AnimationBodyPart.UPPER_BODY);
+        if (Input.GetKeyDown("t")) AnimationSystem.animate(ybot, Animations.SMOKING, AnimationBodyPart.UPPER_BODY);
         if (Input.GetKeyDown("space")) AnimationSystem.resetAnimation(ybot);
         if (Input.GetKeyDown("b")) AnimationSystem.stopAnimation(ybot);
     }
@@ -122,19 +76,30 @@ public class AnimationSystem : MonoBehaviour
 
 public enum Animations
 {
-    NULL = -1,
-    IDLE = 0,
-    RUN = 1,
-    DANCE = 2,
-    DIE = 3, 
-    WALK_MEN = 4, 
-    WALK_WOMEN = 5,
-    WHEELCHAIR1 = 6, 
-    WHEELCHAIR2 = 7, 
-    WALK_WITH_BRIEFCASE = 8,
-    SMOKE = 9,
-    ARGUE1 = 10,
-    ARGUE2 = 11,
-    SIT_AND_TALK_MEN = 12,
-    SIT_AND_TALK_WOMEN = 13,
+    NULL = 0,
+    DYING = 1,
+    IDLE_1 = 2,
+    IDLE_2 = 3,
+    RUNNING_1 = 4,
+    RUNNING_2 = 5,
+    SITTING_1 = 6,
+    SITTING_2 = 7,
+    SMOKING = 8,
+    WHEELCHAIR = 9,
+    TALKING_1 = 10,
+    TALKING_2 = 11,
+    TALKING_3 = 12,
+    TALKING_4 = 13,
+    TALKING_ON_PHONE_1 = 14,
+    TALKING_ON_PHONE_2 = 15,
+    WALKING_1 = 16,
+    WALKING_2 = 17,
+    WALKING_WITH_BRIEFCASE = 18
+}
+
+public enum AnimationBodyPart
+{
+    UPPER_BODY = 1,
+    LOWER_BODY = 2,
+    WHOLE_BODY = 3
 }
