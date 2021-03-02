1c4eb3d65930041c583ad5b675c39b720ea67bc3
Mateusz1010 <lolmanek123456@gmail.com>
2021-03-02 13:36:16 +0100

    It is now made on riging, changing to constrain

--- Susteny/Assets/Animations/work/AnimationSystem.cs
+++ Susteny/Assets/Animations/work/AnimationSystem.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 public class AnimationSystem : MonoBehaviour
 {
@@ -9,14 +10,12 @@ public class AnimationSystem : MonoBehaviour
     void Start()
     {
         animatorController = GetComponent<Animator>().runtimeAnimatorController;
-        test(); // -- testowanie --
+        GameObject ob = new GameObject("ob");
+        ob.transform.position = new Vector3(1, 1, -1.5f);
+        makeLookAt(ybot, ybot); // ----- usuń ----- //
     }
 
-    void Update()
-    {
-    }
-
-    public static void animate(GameObject character, Animations animation)
+    public static Animator prepare(GameObject character)
     {
         Animator characterAnimator;
 
@@ -31,7 +30,76 @@ public class AnimationSystem : MonoBehaviour
             characterAnimator = character.GetComponent<Animator>();
         }
 
+        return characterAnimator;
+    }
+
+    public static RigBuilder prepareRig(GameObject character)
+    {
+        RigBuilder rigBuilder = character.GetComponent<RigBuilder>();
+
+        if (rigBuilder == null)
+        {
+            character.AddComponent<RigBuilder>();
+            GameObject headRig = new GameObject("headRig");
+            headRig.transform.SetParent(character.transform);
+            GameObject bodyRig = new GameObject("bodyRig");
+            bodyRig.transform.SetParent(character.transform);
+
+            headRig.AddComponent<Rig>();
+            bodyRig.AddComponent<Rig>().weight = 0.5f;
+
+            MultiAimConstraint headConstraint = headRig.AddComponent<MultiAimConstraint>();
+            MultiAimConstraint bodyConstraint = bodyRig.AddComponent<MultiAimConstraint>();
+
+            foreach (Transform data in character.GetComponentInChildren<Transform>())
+            {
+                if (data.name == "mixamorig:Hips")
+                {
+                    headConstraint.data.constrainedObject = data.Find("mixamorig:Spine").Find("mixamorig:Spine1").Find("mixamorig:Spine2").Find("mixamorig:Neck").Find("mixamorig:Head");
+                    bodyConstraint.data.constrainedObject = data.Find("mixamorig:Spine");
+                    break;
+                }
+            }
+
+            rigBuilder = character.GetComponent<RigBuilder>();
+
+            rigBuilder.layers.Add(new RigBuilder.RigLayer(headRig.GetComponent<Rig>(),true));
+            rigBuilder.layers.Add(new RigBuilder.RigLayer(bodyRig.GetComponent<Rig>(), true));
+            rigBuilder.Build();
+        }
+
+        return rigBuilder;
+    }
+
+    public static void animate(GameObject character, Animations animation)
+    {
+        Animator characterAnimator = prepare(character);
         characterAnimator.SetInteger("animationIndex", (int) animation);
+        characterAnimator.SetTrigger("endTrigger");
+    }
+
+    public static void resetAnimation(GameObject character)
+    {
+        Animator characterAnimator = prepare(character);
+        characterAnimator.SetTrigger("endTrigger");
+    }
+
+    public static void stopAnimation(GameObject character)
+    {
+        animate(character, Animations.NULL);
+    }
+
+    public static void makeLookAt(GameObject character, GameObject observed)
+    {
+        prepare(character);
+        RigBuilder rigBuilder = prepareRig(character);
+        for (int i = 0; i < rigBuilder.layers.Count; i++)
+        {
+            WeightedTransform source = new WeightedTransform(observed.transform, 1);
+            //rigBuilder.layers[i].rig.GetComponentInParent<MultiAimConstraint>().data.constrainedObject = observed.transform;
+            rigBuilder.layers[i].rig.GetComponentInParent<MultiAimConstraint>().data.sourceObjects.Add(source);
+            rigBuilder.Build();
+        }
     }
 
     // -------------------------------------------------------------------- TESTING AND DEBUGING -------------------------------------------------------------------- //
@@ -40,16 +108,21 @@ public class AnimationSystem : MonoBehaviour
     public GameObject ybot;
     public GameObject henry;
 
-    public void test()
+    void Update()
     {
-        AnimationSystem.animate(xbot, Animations.RUN);
-        AnimationSystem.animate(ybot, Animations.DIE);
-        AnimationSystem.animate(henry, Animations.ARGUE1);
+        if (Input.GetKeyDown("q")) AnimationSystem.animate(ybot, Animations.IDLE);
+        if (Input.GetKeyDown("w")) AnimationSystem.animate(ybot, Animations.RUN);
+        if (Input.GetKeyDown("e")) AnimationSystem.animate(ybot, Animations.DANCE);
+        if (Input.GetKeyDown("r")) AnimationSystem.animate(ybot, Animations.DIE);
+        if (Input.GetKeyDown("t")) AnimationSystem.animate(ybot, Animations.WALK_MEN);
+        if (Input.GetKeyDown("space")) AnimationSystem.resetAnimation(ybot);
+        if (Input.GetKeyDown("b")) AnimationSystem.stopAnimation(ybot);
     }
 }
 
 public enum Animations
 {
+    NULL = -1,
     IDLE = 0,
     RUN = 1,
     DANCE = 2,
@@ -63,5 +136,5 @@ public enum Animations
     ARGUE1 = 10,
     ARGUE2 = 11,
     SIT_AND_TALK_MEN = 12,
-    SIT_AND_TALK_WOMEN = 13
+    SIT_AND_TALK_WOMEN = 13,
 }
