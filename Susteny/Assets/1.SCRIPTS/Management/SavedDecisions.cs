2cca57c0cedbc30af2c92708e54c8abc591dc3af
MikeMG-PL <36279247+MikeMG-PL@users.noreply.github.com>
2021-03-11 08:00:51 +0100

    8.3 SavedDecisions.cs

---
+++ Susteny/Assets/1.SCRIPTS/Management/SavedDecisions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SavedDecisions : MonoBehaviour
+{
+    /// BOOLEANS - DEFINING PLAYER CHOICES (MIGHT BE SAVED TO BINARY IN LATER DEVELOPMENT) ///
+
+    public bool annaDead;
+
+    /// DODATKOWE FUNKCJE (SŁUŻĄCE NP. DO USTAWIANIA WARTOŚCI BOOLI PRZEZ ZEWNĘTRZNE SYSTEMY) ///
+
+    public void setAnnaDead(bool b)
+    {
+        annaDead = b;
+    }
+}
